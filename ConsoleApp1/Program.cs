5ea1a44dc2e69868e8c281f9ad0444c827425b53
Muaaz Naeem <mmuaaz9@gmail.com>
2020-05-19 13:37:38 +0500

    edited main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-//updated form github
+            //updated form github
+            //updated form vs
 
         }
     }
